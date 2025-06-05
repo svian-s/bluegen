78c3f9a0aeabd9c116821fdebdb35e7ca8e35d3d
Asvian S <svians.dev@gmail.com>
2025-06-05 15:11:06 +0700

    Updated login

--- functions/controllers/authController.js
+++ functions/controllers/authController.js
@@ -7,20 +7,20 @@ const pool = require('../db.js');
 require('dotenv').config();
 
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
 
         if (result.rows.length === 0) {
-            return res.status(401).json({ message: 'Invalid email or password' });
+            return res.status(401).json({ message: 'Invalid username or password' });
         }
 
         const user = result.rows[0];
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Invalid email or password' });
+            return res.status(401).json({ message: 'Invalid username or password' });
         }
 
         res.status(200).json({ message: 'Login successful' });
